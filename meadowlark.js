b8f2cde861af5800d968bd7d57ee3424c1b8b71e
voidgogo <2021038@daelim.ac.kr>
2021-10-08 11:48:21 +0900

    Third commit - QA

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,7 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars') // View (frontend)
 // 사용자 정의 라이브러리
 const fortune = require('./lib/fortune.js')
+const handlers = require('./lib/handlers')
 const app = express()
 const port = process.env.PORT || 3000 // port 지정
 
@@ -13,24 +14,12 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-   // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-   // res.render('about', {fortune: randomFortune})
-   res.render('about', {fortune: fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
-app.use((req, res)=>{
-    res.status(404)
-    //res.send('404 - Not Found')
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next)=>{
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port,()=> console.log(`서버 시작 (포트번호 : ${port}, 종료는 Ctrl+c)`) )
