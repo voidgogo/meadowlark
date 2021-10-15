c78f1199ae451db38cc2dd63b0b5d590e8a6ec95
voidgogo <2021038@daelim.ac.kr>
2021-10-15 16:21:11 +0900

    v7 - partial

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,7 @@ const expressHandlebars = require('express-handlebars') // View (frontend)
 // 사용자 정의 라이브러리
 const fortune = require('./lib/fortune.js')
 const handlers = require('./lib/handlers')
+const weatherMiddleware = require('./lib/middleware/weather')
 const app = express()
 const port = process.env.PORT || 3000 // port 지정
 
@@ -23,14 +24,13 @@ app.set('view engine', '.hbs')
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(weatherMiddleware)
+
 app.get('/', handlers.home)
-
 app.get('/about', handlers.about)
-
 app.get('/section-test', handlers.sectionTest)
 
 app.use(handlers.notFound)
-
 app.use(handlers.serverError)
 
 app.listen(port,()=> console.log(`서버 시작 (포트번호 : ${port}, 종료는 Ctrl+c)`) )
